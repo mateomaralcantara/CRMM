6164f8389987e29ef7f1f7d6634efa9ad520837e
rhaiinmigra <162354437+mateomaralcantara@users.noreply.github.com>
2026-09-15 01:13:05 -0400

    feat: add Chat General page

---
+++ app/chat/page.tsx
@@ -0,0 +1,10 @@
+import { AppShell } from "@/components/app-shell";
+import { GeneralChat } from "@/components/general-chat";
+
+export default function ChatPage() {
+  return (
+    <AppShell>
+      <GeneralChat />
+    </AppShell>
+  );
+}
